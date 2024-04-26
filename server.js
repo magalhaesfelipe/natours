bb07fd60550a98e796b8a9bd8caf0d2bceaf225e
lopesmfelipe <felipemlopes0009@gmail.com>
2024-04-26 07:51:01 -0400

    Tour model

--- server.js
+++ server.js
@@ -19,12 +19,24 @@ mongoose
   .then(() => console.log('DB CONNECTION SUCCESSFUL'));
 
 const tourSchema = new mongoose.Schema({
-  
-})
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`App running on PORT ${PORT}!`);
 });
-
-
