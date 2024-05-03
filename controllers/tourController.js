1dffc1c16f98fa060559fda18760009c5676ca6d
lopesmfelipe <felipemlopes0009@gmail.com>
2024-05-03 16:10:05 -0400

    data seed

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -86,7 +86,7 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: 'Invalid data sent!',
+      message: err,
     });
   }
 };
@@ -108,9 +108,7 @@ exports.updateTour = async (req, res) => {
 
     res.status(200).json({
       status: 'success',
-      data: {
-        updatedTour: tour,
-      },
+      data: tour,
     });
   } catch (err) {
     res.status(400).json({
@@ -122,7 +120,7 @@ exports.updateTour = async (req, res) => {
 
 exports.deleteTour = async (req, res) => {
   try {
-    const tour = await Tour.findByIdAndDelete(req.params.id);
+        const tour = await Tour.findByIdAndDelete(req.params.id);
 
     if (!tour) {
       return res.status(404).json({
@@ -133,7 +131,7 @@ exports.deleteTour = async (req, res) => {
 
     res.status(204).json({
       status: 'success',
-      message: 'Resource successfully deleted',
+      data: null,
     });
   } catch (err) {
     res.status(500).json({
