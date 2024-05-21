eecd7b41e77fd74188eaa74fca5efdbe5c41073a
Felipe Lopes <felipemlopes0009@gmail.com>
2024-05-21 04:20:51 -0400

    Pagination getting all Tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,7 +3,7 @@ const Tour = require('../models/tourModel');
 exports.getAllTours = async (req, res) => {
   try {
     // BUILD THE QUERY
-    
+
     // 1A) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
@@ -27,14 +27,27 @@ exports.getAllTours = async (req, res) => {
     // 3) Field limiting
     if (req.query.fields) {
       const fields = req.query.fields.split(',').join(' ');
-      console.log(fields)
+      console.log(fields);
       query = query.select(fields);
     } else {
       query = query.select('-__v');
     }
 
+    // 4)Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    // page=2&limit=10, 1-10 page 1, 11-20 page 2, 21-30 page
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exits');
+    }
+
     // EXECUTE THE QUERY
-    const tours = await query;
+    const tours = await query;  // query.sort().select().skip().limit()
 
     // SEND RESPONSE
     res.status(200).json({
@@ -45,7 +58,7 @@ exports.getAllTours = async (req, res) => {
       },
     });
   } catch (err) {
-    res.status(400).json({
+    res.status(404).json({
       status: 'fail',
       message: err,
     });
