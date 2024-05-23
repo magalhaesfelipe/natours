f6cd98370abccdcd54cc865a58ac240b618d3821
Felipe Lopes <felipemlopes0009@gmail.com>
2024-05-23 05:06:03 -0400

    update

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -10,13 +10,15 @@ exports.aliasTopTours = (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
   try {
-    // EXECUTE THE QUERY
+    // REFINE THE QUERY
     const features = new APIFeatures(Tour.find(), req.query)
       .filter()
       .sort()
       .limitFields()
       .paginate();
-    const tours = await features.query; // query.sort().select().skip().limit()
+
+    //EXECUTE THE QUERY
+    const tours = await features.query;
 
     // SEND RESPONSE
     res.status(200).json({
