e596a849b441c6109f6f2d1d592576a342f747b3
lopesmfelipe <felipemlopes0009@gmail.com>
2024-05-29 00:36:32 -0400

    min & max ratingsAverage and name validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const { default: slugify } = require('slugify');
-const slugigy = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -9,6 +8,8 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
+      maxlength: [40, 'A tour must have less or equal then 40 characters'], // validator only available on strings
+      minlength: [10, 'A tour must have more or equal then 10 characters'],
     },
     slug: String,
     duration: {
@@ -22,6 +23,10 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ["easy", "medium", "difficult"],
+        message: "Difficulty is either: easy, medium, difficult"
+      } 
     },
     price: {
       type: Number,
@@ -30,6 +35,8 @@ const tourSchema = new mongoose.Schema(
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5,'Rating must be bellow 5.0'],
     },
     ratingsQuantity: {
       type: Number,
@@ -90,16 +97,25 @@ tourSchema.post('save', function(doc, next) {
 // QUERY MIDDLEWARE
 tourSchema.pre(/^find/, function (next) {
   this.find({ secretTour: { $ne: true } });
-  
+
   this.start = Date.now();
   next();
 });
 
 tourSchema.post(/^find/, function (docs, next) {
-  console.clear();
-  console.log(`Query took ${Date.now() - this.start} milliseconds`)
+  console.log(`Query took ${Date.now() - this.start} milliseconds`);
   next();
-})
+});
+
+// AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({
+    $match: { secretTour: { $ne: true } },
+  });
+
+  console.log(this);
+  next();
+});
 
 const Tour = mongoose.model('Tour', tourSchema);
 
