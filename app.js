5333f22a86e288f8c5d49814b6bea6b5060bd08b
Felipe Lopes <felipemlopes0009@gmail.com>
2024-06-01 11:15:26 -0400

    Implementing a global error handler middleware and refactoring

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -13,17 +16,14 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-/* app.use((req, res, next) => {
-  console.log('Hello from the middleware 😎');
-  next();
-});
-
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-}); */
-
 // ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404)); // WHENEVER WE PASS ANYTHING TO THE 'NEXT()' FUNCTION, EXPRESS WILL UNDERSTAND THAT THERE IS AN ERROR
+});
+
+app.use(globalErrorHandler);
+
 module.exports = app;
