4948c3fa6593a804a8d9c12b167d60f5b86cc45b
Felipe Lopes <felipemlopes0009@gmail.com>
2024-06-08 02:10:47 -0400

    Modeling users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const userController = require('./../controllers/userController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
+
+router.post('/signup', authController.signup); // in this case it's not 100% fit the REST philosophy 
 
 router
   .route('/')
