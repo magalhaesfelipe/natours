e1a48997e2c32998bb7e201e2038a58f9ddd8453
Felipe Lopes <felipemlopes0009@gmail.com>
2024-06-08 03:20:40 -0400

    Managing passwords

--- models/userModel.js
+++ models/userModel.js
@@ -1,33 +1,49 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
-// name, email, photo, password, passwordConfirm
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A user must have name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Plase confirm your password'],
+    validate: {
+      // THIS ONLY WORKS ON CREATE AND SAVE
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Password are not the same!',
+    },
+  },
+});
 
-const userSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: [true, 'A user must have name']
-    },
-    email: {
-      type: String,
-      required: [true, 'Please provide your email'],
-      unique: true,
-      lowercase: true,
-      validate: [validator.isEmail, 'Please provide a valid email']
-    },
-    photo: String,
-    password: {
-      type: String,
-      required: [true, 'Please provide a password'],
-      minlength: 8
-    },
-    passwordConfirm: {
-      type: String,
-      required: [true, 'Plase confirm your password']
-    }
-  }
-)
+userSchema.pre('save', async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete passwordConfirm field 
+  this.passwordConfirm = undefined;
+  next();
+});
 
 const User = mongoose.model('User', userSchema);
 
