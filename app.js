579a687647b6cd6c42a46893a332a0f43426828f
Felipe Lopes <felipemlopes0009@gmail.com>
2024-06-12 16:56:21 -0400

    Getting token and check of it's there

--- app.js
+++ app.js
@@ -9,18 +9,16 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // MIDDLEWARES
-/* if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
-} */
-
-app.use(morgan('dev'));
+}
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-
+  // console.log(req.headers);
   next();
 });
 
